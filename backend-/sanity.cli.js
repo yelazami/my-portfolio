30345e28b66c7583bf2f3c409b2d9001c275191b
YassOS <el.yassine.azami@gmail.com>
2023-01-13 20:18:00 +0100

    features/portoflio-front

---
+++ backend-/sanity.cli.js
@@ -0,0 +1,8 @@
+import {defineCliConfig} from 'sanity/cli'
+
+export default defineCliConfig({
+  api: {
+    projectId: 'r1eur88a',
+    dataset: 'production'
+  }
+})
---
+++ frontend/src/components/SocialMedia.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import { BsTwitter, BsInstagram } from 'react-icons/bs';
+import { FaFacebookF, FaLinkedinIn } from 'react-icons/fa';
+
+const SocialMedia = () => (
+  <div className="app__social">
+    <div>
+      <a href="https://www.linkedin.com/in/yassine-el-azami/" target='_blank'>
+        <FaLinkedinIn />
+      </a>
+    </div>
+    <div>
+      <a href="https://twitter.com" target='_blank'>
+        <BsTwitter />
+      </a>
+    </div>
+    <div>
+      <a href="https://www.facebook.com/Mr.YassineL/" target='_blank'>
+        <FaFacebookF />
+      </a>
+    </div>
+    <div>
+      <a href="https://www.instagram.com/l_azami_yassine_/" target='_blank'>
+        <BsInstagram />
+      </a>
+    </div>
+  </div>
+);
+
+export default SocialMedia;
